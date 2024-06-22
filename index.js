6a1a46472e5ac15cdc44991c627ddd29568c8f44
rakibulhasan044 <rhrakib044@gmail.com>
2024-06-22 23:47:27 +0600

    all meals route

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.use(express.json());
 console.log(`DB_USER: ${process.env.DB_USER}`);
 console.log(`DB_PASS: ${process.env.DB_PASS}`);
 
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erh7g8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erh7g8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +28,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const mealsCollection = client.db("uniBitesDB").collection('meals');
+
+    app.get('/meals', async (req, res) => {
+        const result = await mealsCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
