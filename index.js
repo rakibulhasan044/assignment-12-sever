d42e73fd9821a46e75da899b26670288b8a80628
rakibulhasan044 <rhrakib044@gmail.com>
2024-07-05 07:20:41 +0600

    package api added

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 6006;
 
 const corsOption = {
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://assignment-20dc7.web.app',
-      
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://assignment-20dc7.web.app",
   ],
-    credentials: true,
-  }
-  
-  app.use(cors(corsOption));
+  credentials: true,
+};
+
+app.use(cors(corsOption));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erh7g8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,89 +33,94 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const mealsCollection = client.db("uniBitesDB").collection('meals');
-    const usersCollection = client.db('uniBitesDB').collection('users');
+    const mealsCollection = client.db("uniBitesDB").collection("meals");
+    const usersCollection = client.db("uniBitesDB").collection("users");
+    const packageCollection = client.db("uniBitesDB").collection('package')
 
     // app.get('/meals', async (req, res) => {
     //     const result = await mealsCollection.find().toArray();
     //   res.send(result)
     // })
 
-app.get('/meals', async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 0; 
-  const skip = (page - 1) * limit;
+    app.get("/meals", async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 0;
+      const skip = (page - 1) * limit;
 
-  const filter = req.query.filter;
-  const priceRange = req.query.priceRange;
-  let query = {};
+      const filter = req.query.filter;
+      const priceRange = req.query.priceRange;
+      let query = {};
 
-  if (filter) {
-    query.category = filter;
-  }
+      if (filter) {
+        query.category = filter;
+      }
 
-  if (priceRange) {
-    const [min, max] = priceRange.split('-').map(Number);
-    query.price = { $gte: min, $lte: max };
-  }
+      if (priceRange) {
+        const [min, max] = priceRange.split("-").map(Number);
+        query.price = { $gte: min, $lte: max };
+      }
 
-  const result = limit > 0 
-    ? await mealsCollection.find(query).skip(skip).limit(limit).toArray()
-    : await mealsCollection.find(query).toArray();
+      const result =
+        limit > 0
+          ? await mealsCollection.find(query).skip(skip).limit(limit).toArray()
+          : await mealsCollection.find(query).toArray();
 
-  res.send(result);
-});
+      res.send(result);
+    });
 
-app.get("/mealsCount", async (req, res) => {
-  const filter = req.query.filter;
-  const priceRange = req.query.priceRange;
-  let query = {};
-  
-  if (filter) query = { category: filter };
-  if (priceRange) {
-    const [min, max] = priceRange.split('-').map(Number);
-    query.price = { $gte: min, $lte: max };
-  }
+    app.get("/mealsCount", async (req, res) => {
+      const filter = req.query.filter;
+      const priceRange = req.query.priceRange;
+      let query = {};
 
-  const count = await mealsCollection.countDocuments(query);
-  res.send({ count });
-});
+      if (filter) query = { category: filter };
+      if (priceRange) {
+        const [min, max] = priceRange.split("-").map(Number);
+        query.price = { $gte: min, $lte: max };
+      }
 
-    app.get('/meal/:id', async (req, res) => {
+      const count = await mealsCollection.countDocuments(query);
+      res.send({ count });
+    });
+
+    app.get("/meal/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await mealsCollection.findOne(query);
       res.send(result);
-    })
+    });
 
     //save new user in db
 
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
-      const isExist = await usersCollection.findOne(query)
+      const query = { email: user.email };
+      const isExist = await usersCollection.findOne(query);
 
-      if(isExist) {
-        return res.send({ message: 'user already exist', insertedId: null})
+      if (isExist) {
+        return res.send({ message: "user already exist", insertedId: null });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
+    });
+
+
+    app.get('/package', async (req, res) => {
+        const result = await packageCollection.find().toArray();
+      res.send(result)
     })
 
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-
-app.get('/',(req, res) => {
-    res.send('12 server')
-})
+app.get("/", (req, res) => {
+  res.send("12 server");
+});
 
 app.listen(port, () => {
-    console.log(`12 server ${port}`);
-})
+  console.log(`12 server ${port}`);
+});
